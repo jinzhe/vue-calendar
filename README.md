ba2f63b4a633772f6f6c36af6c9d56bb6df078ad
ZEE.KIM <129@jinzhe.net>
2015-12-22 17:45:10 +0800

    update151222

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # vue-calendar
-Calendar Components for Vue.js.
+
+基于vuejs+webpack环境使用的日期选择插件
+
+1.支持单选多选日期
+2.支持限定开始和结束日期范围选择。
+3.支持小时分钟
+4.需要引入fontawesome.io 的图标库。
+
+QQ群: 240319632
+
+![](http://ww1.sinaimg.cn/large/823603acgw1ez1n57t8jug20d40acwjw.gif)
+
+![](http://ww3.sinaimg.cn/large/823603acgw1ez8ks5cudrg20in0badzn.gif)
+
+
+# Options
+:show    是否显示
+:type    date|datetime
+:value	默认值
+:begin  可选开始时间
+:end    可选结束时间
+:x      显示x坐标
+:y      显示y坐标
+:range  是否多选
+
+# test.vue
+
+```
+<template>
+	<input type="text" @click="showCalendar" v-model="value" placeholder="请输入日期">
+	<calendar :show.sync="show" :value.sync="value" :x="x" :y="y" :begin="begin" :end="end" :range="range"></calendar>
+</template>
+
+<script>
+module.exports = {
+    data: function() {
+        return {
+            show:false,
+            type:"date", //date datetime
+            value:"2015-12-11",
+            begin:"2015-12-20",
+            end:"2015-12-25",
+            x:0,
+            y:0,
+            range:true,//是否多选
+        }
+    },
+    methods:{
+        showCalendar:function(e){
+            e.stopPropagation();
+            var that=this;
+            that.show=true;
+            that.x=e.target.offsetLeft;
+            that.y=e.target.offsetTop+e.target.offsetHeight+8;
+            var bindHide=function(e){
+                e.stopPropagation();
+                that.show=false;
+                document.removeEventListener('click',bindHide,false);
+            };
+            setTimeout(function(){
+                document.addEventListener('click',bindHide,false);
+            },500);
+        }
+    },
+    components:{
+        calendar: require('./calendar.vue')
+    }
+}
+</script>
+```
--- calendar.vue
+++ calendar.vue
@@ -1,11 +1,15 @@
 <template>
-    <div @click="none"  @touchstart="none" class="calendar" v-show="show" :style="{'left':x+'px','top':y+'px'}" transition="calendar" transition-mode="out-in">
+    <div @click.stop=""  @touchstart.stop="" class="calendar" v-show="show" :style="{'left':x+'px','top':y+'px'}" transition="calendar" transition-mode="out-in">
         <div class="calendar-tools">
             <i class="fa fa-angle-left float left" @click="prev"  @touchstart="prev"></i>
             <i class="fa fa-angle-right float right" @click="next"  @touchstart="next"></i>
-            <div class="text center">{{current}}</div>
+            <div class="text center">
+                <input type="number" v-model="year" value="{{year}}" @change="render(year,month)" min="1970" max="2100" maxlength="4" number>
+                 / 
+                {{month+1}}
+            </div>
         </div>
-        <table>
+        <table cellpadding="5">
         <thead>
             <tr>
                 <td v-for="week in weeks" class="week">{{week}}</td>
@@ -17,9 +21,25 @@
             :class="{'today':child.today,'disabled':child.disabled}"
             @click="select(k1,k2,$event)" @touchstart="select(k1,k2,$event)">
             {{child.day}}
+            <div class="lunar" v-if="showLunar">{{child.lunar}}</div>
             </td>
         </tr>
         </table>
+        <div class="calendar-time" v-show="type=='datetime'||type=='time'">
+ 
+            <div class="timer">
+                <input type="number" v-model="hour" value="{{hour}}" min="0" max="23" maxlength="2" number>
+                时
+                <input type="number" v-model="minute" value="{{minute}}" min="0" max="59" maxlength="2" number>
+                分
+                <input type="number" v-model="second" value="{{second}}" min="0" max="59" maxlength="2" number>
+                秒
+            </div>
+        </div>
+        <div class="calendar-button" v-show="type=='datetime'||type=='time'||range">
+            <button @click="ok">确定</button>
+            <button @click="cancel" class="cancel">取消</button>
+        </div>
     </div>
 </template>
 
@@ -31,6 +51,10 @@ module.exports = {
             required: true,
             twoWay: true    
         },
+        type: {
+            type: String,
+            required: true  
+        },
         value: {
             type: String,
             required: true,
@@ -38,19 +62,26 @@ module.exports = {
         },
         x: {
             type: Number,
-            required: true,
-            twoWay: true    
+            required: true  
         },
         y: {
             type: Number,
-            required: true,
-            twoWay: true    
+            required: true  
         }, 
         begin: {
             type: String
         }, 
         end: {
             type: String 
+        },
+        range: {
+            type: Boolean
+        },
+        rangeBegin:{
+            type: Array
+        },
+        rangeEnd:{
+            type: Array
         }
     },
     data:function(){
@@ -58,9 +89,12 @@ module.exports = {
             year:0,
             month:0,
             day:0,
+            hour:0,
+            minute:0,
+            second:0,
             days:[],
             today:[],
-            current:'',
+            currentMonth:Number,
             sep:"/",
             weeks:['日', '一', '二', '三', '四', '五', '六'],
             months:['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
@@ -69,23 +103,66 @@ module.exports = {
     ready:function(){
         var that=this;
         var now = new Date();
-        if(that.value.indexOf("-")!=-1)that.sep="-";
-        that.year = now.getFullYear();
-        that.month = now.getMonth();
-        that.day = now.getDate();
+        if(that.value!=""){
+            if(that.value.indexOf("-")!=-1)that.sep="-";
+            if(that.value.indexOf(".")!=-1)that.sep=".";
+ 
+            if(that.type=="date"){
+                var split=that.value.split(that.sep);
+                that.year = parseInt(split[0]);
+                that.month =parseInt(split[1])-1;
+                that.day = parseInt(split[2]);
+            }else if(that.type=="datetime"){
+                var split=that.value.split(" ");
+                var splitDate=split[0].split(that.sep);
+                that.year = parseInt(splitDate[0]);
+                that.month =parseInt(splitDate[1])-1;
+                that.day = parseInt(splitDate[2]);
+                if(split.length>1){
+                    var splitTime=split[1].split(":");
+                    that.hour = splitTime[0];
+                    that.minute = splitTime[1];
+                    that.second = splitTime[2];
+                }
+            }
+            if(that.range){
+                var split=that.value.split(" ~ ");
+                if(split.length>1){
+                    var beginSplit=split[0].split(that.sep);
+                    var endSplit=split[1].split(that.sep);
+                    that.rangeBegin=[parseInt(beginSplit[0]),parseInt(beginSplit[1]-1),parseInt(beginSplit[2])];
+                    that.rangeEnd=[parseInt(endSplit[0]),parseInt(endSplit[1]-1),parseInt(endSplit[2])];
+                }
+            }
+        }else{
+            that.year = now.getFullYear();
+            that.month =now.getMonth();
+            that.day = now.getDate();
+            that.hour = that.zero(now.getHours());
+            that.minute = that.zero(now.getMinutes());
+            that.second = that.zero(now.getSeconds());
+            if(that.range){
+                that.rangeBegin=[];
+                that.rangeEnd=[];
+            }
+        }
         that.render(that.year,that.month);
+
     },
     methods:{
+        zero:function(n){
+            return n<10?'0'+n:n;
+        },
         render:function(y,m){
             var that=this;
             var d = new Date(),
             firstDayOfMonth = new Date(y, m, 1).getDay(),//当月第一天
             lastDateOfMonth = new Date(y, m + 1, 0).getDate(),//当月最后一天
             lastDayOfLastMonth = m == 0 ? new Date(y - 1, 11, 0).getDate() : new Date(y, m, 0).getDate();//最后一月的最后一天
-            that.current=y + ' / ' + that.months[m];
+            that.year=y;
+            that.currentMonth=that.months[m];
 
-
-            var seletSplit=that.value.split(that.sep);
+            var seletSplit=that.value.split(" ")[0].split(that.sep);
             var i,line=0,temp=[];
             for(i=1;i <= lastDateOfMonth;i++) {
                 var dow = new Date(y, m, i).getDay();
@@ -101,47 +178,61 @@ module.exports = {
                         k++;
                     }
                 }
-                // 当月渲染
-                var chk = new Date();
-                var chkY = chk.getFullYear();
-                var chkM = chk.getMonth();
-                // 选中当前input日期
-                if(
-                    parseInt(seletSplit[0])==that.year 
-                    &&
-                    parseInt(seletSplit[1])-1==that.month
-                    &&
-                    parseInt(seletSplit[2])==i
-                    &&
-                    (that.begin==""&&that.end=="")
-                ){
-                    temp[line].push({day:i,today:true});
-                    that.today=[line,temp[line].length-1];
-                // 当天
-                }else if (chkY == that.year && chkM == that.month && i == that.day&&that.value=="") {
-                    temp[line].push({day:i,today:true});
-                    that.today=[line,temp[line].length-1];
-                } else {//默认
-                    // 1.判断begin和end的日期
-                    var options={day:i,today:false};
-                    if(that.begin!=""){
-                        var beginSplit=that.begin.split(that.sep);
-                        var beginDate=new Date(parseInt(beginSplit[0]),parseInt(beginSplit[1])-1,parseInt(beginSplit[2]));
-                        var beginTime=Number(beginDate);
-                        var thisDate=new Date(that.year,that.month,i);
-                        var thisTime=Number(thisDate);
-                        if(beginTime>thisTime)options.disabled=true;
-                    }
-                    if(that.end!=""){
-                        var endSplit=that.end.split(that.sep);
-                        var endDate=new Date(parseInt(endSplit[0]),parseInt(endSplit[1])-1,parseInt(endSplit[2]));
-                        var endTime=Number(endDate);
-                        var thisDate=new Date(that.year,that.month,i);
-                        var thisTime=Number(thisDate);
-                        if(endTime<thisTime)options.disabled=true;
+ 
+                // 如果是日期范围
+                if(that.range){
+                      
+                    var options={day:i};
+       
+                    if(that.rangeBegin.length>0){
+                        var beginTime=Number(new Date(that.rangeBegin[0],that.rangeBegin[1],that.rangeBegin[2]));
+                        var endTime=Number(new Date(that.rangeEnd[0],that.rangeEnd[1],that.rangeEnd[2]));
+                        var thisTime=Number(new Date(that.year,that.month,i));
+ 
+                        if(beginTime<=thisTime&&endTime>=thisTime){
+                            options.today=true;
+                        }
                     }
                     temp[line].push(options);
+                }else{
+                   
+                    var chk = new Date();
+                    var chkY = chk.getFullYear();
+                    var chkM = chk.getMonth();
+                    if(
+                        parseInt(seletSplit[0])==that.year 
+                        &&
+                        parseInt(seletSplit[1])-1==that.month
+                        &&
+                        parseInt(seletSplit[2])==i
+                        &&
+                        (that.begin==undefined||that.end==undefined)
+                    ){
+                        temp[line].push({day:i,today:true});
+                        that.today=[line,temp[line].length-1];
+                    // 当天
+                    }else if (chkY == that.year && chkM == that.month && i == that.day&&that.value==undefined) {
+                        temp[line].push({day:i,today:true});
+                        that.today=[line,temp[line].length-1];
+                    } else {//默认
+                        // 1.判断begin和end的日期
+                        var options={day:i,today:false};
+                        if(that.begin!=undefined){
+                            var beginSplit=that.begin.split(that.sep);
+                            var beginTime=Number(new Date(parseInt(beginSplit[0]),parseInt(beginSplit[1])-1,parseInt(beginSplit[2])));
+                            var thisTime=Number(new Date(that.year,that.month,i));
+                            if(beginTime>thisTime)options.disabled=true;
+                        }
+                        if(that.end!=undefined){
+                            var endSplit=that.end.split(that.sep);
+                            var endTime=Number(new Date(parseInt(endSplit[0]),parseInt(endSplit[1])-1,parseInt(endSplit[2])));
+                            var thisTime=Number(new Date(that.year,that.month,i));
+                            if(endTime<thisTime)options.disabled=true;
+                        }
+                        temp[line].push(options);
+                    }   
                 }
+               
                 // 最后一行
                 if (dow == 6) {
                     line++;
@@ -152,8 +243,7 @@ module.exports = {
                         k++;
                     }
                 }
-            }
-
+            }//end for
             that.days=temp;
         },
         prev:function(e){
@@ -181,19 +271,65 @@ module.exports = {
         select:function(k1,k2,e){
             if(e!=undefined)e.stopPropagation();
             var that=this;
-            // 取消上次选中
-            if(that.today.length>0){
-                that.days[that.today[0]][that.today[1]].today=false; 
-            }
+
             
-            // 设置当前选中天
-            that.days[k1][k2].today=true;
-            that.today=[k1,k2];
-            that.value=that.year+that.sep+(that.month+1)+that.sep+that.days[k1][k2].day;
+            // 日期范围
+            if(that.range){
+                if(that.rangeBegin.length==0||that.rangeEndTemp!=0){
+                    that.rangeBegin=[that.year,that.month,that.days[k1][k2].day,that.hour,that.minute,that.second];
+                    that.rangeBeginTemp=that.rangeBegin;
+                    that.rangeEnd=[that.year,that.month,that.days[k1][k2].day,that.hour,that.minute,that.second];
+                    that.rangeEndTemp=0;
+                }else{
+                    that.rangeEnd=[that.year,that.month,that.days[k1][k2].day,that.hour,that.minute,that.second];
+                    that.rangeEndTemp=1;
+                    // 判断结束日期小于开始日期则自动颠倒过来
+                    if(+new Date(that.rangeEnd[0],that.rangeEnd[1],that.rangeEnd[2])<+new Date(that.rangeBegin[0],that.rangeBegin[1],that.rangeBegin[2])){
+                        that.rangeBegin=that.rangeEnd;
+                        that.rangeEnd=that.rangeBeginTemp;
+                    }
+                }
+                that.render(that.year,that.month);
+            }else{
+                // 取消上次选中
+                if(that.today.length>0){
+                    that.days[that.today[0]][that.today[1]].today=false; 
+                }
+                 // 设置当前选中天
+                that.days[k1][k2].today=true;
+                that.day=that.days[k1][k2].day;
+                that.today=[k1,k2];
+                if(that.type=='date'){
+                    that.value=that.year+that.sep+that.zero(that.month+1)+that.sep+that.zero(that.days[k1][k2].day);
+                    that.show=false;   
+                }
+            }
+
+        },
+        ok:function(){
+            var that=this;
+            if(that.range){
+                that.value=that.output(that.rangeBegin)+" ~ "+that.output(that.rangeEnd);
+            }else{
+                that.value=that.output([that.year,that.month,that.day,that.hour,that.minute,that.second]);
+            }
             that.show=false;
         },
-        none:function(e){
-            e.stopPropagation();
+        cancel:function(){
+            this.show=false;
+        },
+        // 格式化输出
+        output:function(args){
+            var that=this;
+            if(that.type=='time'){
+                return that.zero(args[3])+":"+that.zero(args[4])+":"+that.zero(args[5])
+            }
+            if(that.type=='datetime'){
+                return args[0]+that.sep+that.zero(args[1]+1)+that.sep+that.zero(args[2])+" "+that.zero(args[3])+":"+that.zero(args[4])+":"+that.zero(args[5])
+            }
+            if(that.type=='date'){
+                return args[0]+that.sep+that.zero(args[1]+1)+that.sep+that.zero(args[2]);
+            }
         }
     }
 }
@@ -239,6 +375,16 @@ module.exports = {
     line-height: 32px;
     color:#5e7a88;
 }
+
+.calendar-tools input{
+    font-size: 20px;
+    line-height: 32px;
+    color: #5e7a88;
+    width: 70px;
+    text-align: center;
+    border:none;
+    background-color: transparent;
+}
 .calendar-tools>i{
     margin:0 16px;
     line-height: 32px;
@@ -249,39 +395,89 @@ module.exports = {
     color:#5e7a88;
 }
 .calendar table {
-  clear: both;
-  width: 100%;
-  border-collapse: collapse;
-  color: #444444;
+    clear: both;
+    width: 100%;
+    margin-bottom:10px;
+    border-collapse: collapse;
+    color: #444444;
 }
 .calendar td {
-  height: 40px;
-  width: 14.28571429%;
-  text-align: center;
-  vertical-align: middle;
-  font-size:14px;
-  cursor: pointer;
-  border-radius: 2px;
+    margin:2px !important;
+    padding:8px 0;
+    width: 14.28571429%;
+    text-align: center;
+    vertical-align: middle;
+    font-size:16px;
+    line-height: 125%;
+    cursor: pointer;
 }
 .calendar td:hover{
     background:#f3f8fa;
 }
 .calendar td.week{
-  pointer-events:none !important;
-  cursor: default !important;    
+    pointer-events:none !important;
+    cursor: default !important;    
 }
 .calendar td.disabled {
-  color: #c0c0c0;
-  pointer-events:none !important;
-  cursor: default !important;
+    color: #c0c0c0;
+    pointer-events:none !important;
+    cursor: default !important;
 }
 .calendar td.today {
-    
-  background-color: #5e7a88;
-  color: #fff;
-  font-size:16px;
+    background-color: #5e7a88;
+    color: #fff;
+    font-size:16px;
 }
 .calendar thead td {
   text-transform: uppercase;
+}
+.calendar .timer{
+    margin:10px 0;
+    text-align: center;
+}
+.calendar .timer input{
+    border-radius: 2px;
+    padding:5px;
+    font-size: 14px;
+    line-height: 18px;
+    color: #5e7a88;
+    width: 50px;
+    text-align: center;
+    border:1px solid #efefef;
+}
+.calendar .timer input:focus{
+    border:1px solid #5e7a88;
+}
+.calendar-button{
+    text-align: center;
+}
+
+.calendar-button button{
+    cursor: pointer;
+    display: inline-block;
+    min-height: 1em;
+    min-width: 8em;
+    vertical-align: baseline;
+    background:#5e7a88;
+    color:#fff;
+    margin: 0 .25em 0 0;
+    padding: .8em 2.5em;
+    font-size: 1em;
+    line-height: 1em;
+    text-align: center;
+    border-radius: .3em;
+}
+.calendar-button button.cancel{
+    background:#efefef;
+    color:#666;
+}
+
+.calendar .lunar{
+     font-size:11px;
+     line-height: 150%;
+     color:#aaa;   
+}
+.calendar td.today .lunar{
+     color:#fff;   
 }
 </style>
