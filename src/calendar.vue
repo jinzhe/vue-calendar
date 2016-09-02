4da2abc48bf4883e330a56f9de9dcd283bb72882
ZEE.KIM <129@jinzhe.net>
2016-09-02 17:09:24 +0800

    修复移动端点击2次问题

--- src/calendar.vue
+++ src/calendar.vue
@@ -158,13 +158,13 @@
 </style>
 
 <template>
-    <div @click.stop=""  @touchstart.stop="" class="calendar" v-show="show" :style="{'left':x+'px','top':y+'px'}" transition="calendar" transition-mode="out-in">
+    <div @click.stop=""  class="calendar" v-show="show" :style="{'left':x+'px','top':y+'px'}" transition="calendar" transition-mode="out-in">
         <div  v-if="type!='time'">
             <div class="calendar-tools">
-                <span class="calendar-prev" @click="prev" @touchstart="prev">
+                <span class="calendar-prev" @click="prev">
                     <svg width="16" height="16" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g class="transform-group"><g transform="scale(0.015625, 0.015625)"><path d="M671.968 912c-12.288 0-24.576-4.672-33.952-14.048L286.048 545.984c-18.752-18.72-18.752-49.12 0-67.872l351.968-352c18.752-18.752 49.12-18.752 67.872 0 18.752 18.72 18.752 49.12 0 67.872l-318.016 318.048 318.016 318.016c18.752 18.752 18.752 49.12 0 67.872C696.544 907.328 684.256 912 671.968 912z" fill="#5e7a88"></path></g></g></svg>
                 </span>
-                <span class="calendar-next"  @click="next" @touchstart="next">
+                <span class="calendar-next"  @click="next">
                     <svg width="16" height="16" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g class="transform-group"><g transform="scale(0.015625, 0.015625)"><path d="M761.056 532.128c0.512-0.992 1.344-1.824 1.792-2.848 8.8-18.304 5.92-40.704-9.664-55.424L399.936 139.744c-19.264-18.208-49.632-17.344-67.872 1.888-18.208 19.264-17.376 49.632 1.888 67.872l316.96 299.84-315.712 304.288c-19.072 18.4-19.648 48.768-1.248 67.872 9.408 9.792 21.984 14.688 34.56 14.688 12 0 24-4.48 33.312-13.44l350.048-337.376c0.672-0.672 0.928-1.6 1.6-2.304 0.512-0.48 1.056-0.832 1.568-1.344C757.76 538.88 759.2 535.392 761.056 532.128z" fill="#5e7a88"></path></g></g></svg>
                 </span>
                 <div class="text center">
@@ -287,7 +287,6 @@ export default {
         }
     },
     created() {
-        console.log("created")
         this.init()
         // 延迟绑定事件，防止关闭
         window.setTimeout(() => {
